2ee5f33f78f351d1ed4bf87f632cb0ec9413cc7c
George Townley <george@knowlesavenue.com>
2024-02-18 00:44:20 +0000

    added animations to script

--- Player.cs
+++ Player.cs
@@ -5,13 +5,20 @@ public partial class Player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	private AnimatedSprite2D animatedSprite;
+
+	public override void _Ready()
+{
+	base._Ready();
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	// ... any other initialization code ...
+}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		 Vector2 velocity = Velocity;
+		Vector2 velocity = Velocity;
 
 		// Add the gravity.
 		if (!IsOnFloor())
@@ -22,7 +29,6 @@ public partial class Player : CharacterBody2D
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
@@ -31,6 +37,16 @@ public partial class Player : CharacterBody2D
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+		
+		// Animation state
+		if (!IsOnFloor())
+		{
+			animatedSprite.Play("jump");
+		}
+		else if (IsOnFloor() && velocity.Length() == 0)
+		{
+			animatedSprite.Play("idle");
 		}
 
 		Velocity = velocity;
