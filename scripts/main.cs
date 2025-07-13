10720de145861a698c1f3e5da980cfa1625e0596
George Townley <george@knowlesavenue.com>
2025-07-13 11:05:52 +0100

    add debug settings

--- scripts/main.cs
+++ scripts/main.cs
@@ -3,13 +3,19 @@ using System;
 
 public partial class main : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-
-	public override void _Process(double delta)
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        if (OS.IsDebugBuild())
+        {
+            var window = GetWindow();
+            window.Mode = Window.ModeEnum.Windowed;
+            window.Size = new Vector2I(1280, 720);
+            window.Position = new Vector2I(100, 100); // Optional: set position
+        }
+    }
+    
+    public override void _Process(double delta)
+    {
+    }
 }
